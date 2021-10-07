10018dc3e4dd48ac2482609ba925daf5f576ca9e
iharea <iharea@pentalog.com>
2021-10-07 12:32:52 +0300

    :construction: Move the previous logic outside to own module and use make use of nest's .env configuration module.

--- roadmap-task-api/src/app.controller.ts
+++ roadmap-task-api/src/app.controller.ts
@@ -1,35 +1,4 @@
-import {
-  Controller,
-  Get,
-  InternalServerErrorException,
-  Param,
-} from '@nestjs/common';
-import { AppService } from './app.service';
-import { FindOneParams } from './pipes/findOneParams';
+import { Controller } from '@nestjs/common';
 
 @Controller()
-export class AppController {
-  constructor(private readonly appService: AppService) {}
-
-  @Get('/users')
-  async getUsers() {
-    const users = await this.appService.getUsers();
-
-    if (users) return users;
-
-    throw new InternalServerErrorException(
-      'Oops, something went wrong, could not fetch the data form the external API',
-    );
-  }
-
-  @Get('/users/:id')
-  async getUser(@Param() params: FindOneParams) {
-    const user = await this.appService.getUser(params.id);
-
-    if (user) return user;
-
-    throw new InternalServerErrorException(
-      'Oops, something went wrong, could not fetch the data form the external API',
-    );
-  }
-}
+export class AppController {}
--- roadmap-task-api/src/app.service.ts
+++ roadmap-task-api/src/app.service.ts
@@ -1,24 +1,4 @@
 import { Injectable } from '@nestjs/common';
-import axios from 'axios';
-
-const TYPICODE_URL = 'https://jsonplaceholder.typicode.com';
 
 @Injectable()
-export class AppService {
-  getUser = async (id: number) => {
-    try {
-      const { data } = await axios.get(`${TYPICODE_URL}/users/${id}`);
-      return data;
-    } catch {
-      return undefined;
-    }
-  };
-  getUsers = async () => {
-    try {
-      const { data } = await axios.get(`${TYPICODE_URL}/users`);
-      return data;
-    } catch {
-      return undefined;
-    }
-  };
-}
+export class AppService {}
